1219c294623bb601756b89c579f9558098c05010
16/10/2023 <jwieczor@op.pl>
2023-10-16 18:37:16 +0200

    pierwsze zajecia

--- src/main/java/second/shortcuts/ClassThatHaveItAll.java
+++ src/main/java/second/shortcuts/ClassThatHaveItAll.java
@@ -1,4 +1,43 @@
 package second.shortcuts;
 
-public class ClassThatHaveItAll implements InterfaceOne {String name;Integer number;@Override
-    public void printMe(String info) {  } }
+import java.util.List;
+
+public class ClassThatHaveItAll implements InterfaceOne {
+    String name;
+    List<Long> lista;
+    Integer number;
+
+    public ClassThatHaveItAll(String name, List<Long> lista, Integer number) {
+        this.name = name;
+        this.lista = lista;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Long> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Long> lista) {
+        this.lista = lista;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public void printMe(String info) {
+    }
+}
